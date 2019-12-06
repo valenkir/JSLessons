377bef067c643f1baa522b53f4f2216944e19ef5
Valentyna Boriak <valenkiri@gmail.com>
2019-12-06 14:14:49 +0200

    Added the isNatural function to the function_pow file

--- Lesson-Functions/function_pow.js
+++ Lesson-Functions/function_pow.js
@@ -1,7 +1,18 @@
 function pow(x, n){
-    let result = 1;
-    for(let i=0; i<n; i++){
-        result*=x;
+    if(isNatural(n)){
+        let result = 1;
+        for(let i=0; i<n; i++){
+            result*=x;
+        }
+        return result;
+    } else {
+        return "Wront value!";
     }
-    return result;
+}
+
+function isNatural(n){
+    if (n < 1 || n%1!=0){
+        return false;
+    }
+    return true;
 }
