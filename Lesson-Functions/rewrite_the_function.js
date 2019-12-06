5a21b6577de55ee8a9a7b7f912963ce621b796a1
Valentyna Boriak <valenkiri@gmail.com>
2019-12-06 13:17:35 +0200

    The Functions lesson

---
+++ Lesson-Functions/rewrite_the_function.js
@@ -0,0 +1,11 @@
+//1
+
+  function checkAgeTernary(age){
+      return age > 18 ? true : confirm('Did parents allow you?');
+  }
+
+//2
+
+function checkAgeOR(age){
+    return (age>18) || confirm('Did parents allow you?');
+}
