5eecc18d46ad2e1cd300c1f6f7e8dcf328fb2f3a
Valentyna Boriak <valenkiri@gmail.com>
2019-12-06 11:52:43 +0200

    The Loops lesson

---
+++ Lesson-Loops/output_prime_numbers.js
@@ -0,0 +1,18 @@
+let number = prompt("Enter the number greater than 2");
+
+if(number>=2){
+    for (let i=2; i<=number; i++){
+        let isPrime = true;
+        for (let j=2; j<i; j++){
+            if(i%j == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            alert(i);
+        }
+    }
+} else {
+    alert("Wrong number!");
+}
